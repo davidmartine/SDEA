50c77eb3dcd680b560aa5850fa48ff0d698786a4
davidmartine <auxiliarti01@ecar.com.co>
2022-10-15 11:26:55 -0500

    creacion menu perfil operador

---
+++ SDEAXamarin/SDEAXamarin/SDEAXamarin/Model/MMenuOperador.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SDEAXamarin.Model
+{
+    public  class MMenuOperador
+    {
+        public string Descripcion { get; set; }
+        public string Imagen { get; set; }
+
+    }
+}
