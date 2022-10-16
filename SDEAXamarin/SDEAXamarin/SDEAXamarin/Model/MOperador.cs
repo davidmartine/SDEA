162a6e107c22700e18ed35d501f2103e5a267cbb
davidmartine <auxiliarti01@ecar.com.co>
2022-10-15 20:18:39 -0500

    Agregando funcion Insertar Incidente

---
+++ SDEAXamarin/SDEAXamarin/SDEAXamarin/Model/MOperador.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SDEAXamarin.Model
+{
+    public class MOperador
+    {
+        public string Accion { get; set; }
+        public string Cedula { get; set; }
+        public string Descripcion { get; set; }
+        public string Edad { get; set; }
+        public string Eps { get; set; }
+        public string Evento { get; set; }
+        public string FechaNacimiento { get; set; }
+        public string Nombre { get; set; }
+        public string Telefono { get; set; }
+
+    }
+}
--- SDEAXamarin/SDEAXamarin/SDEAXamarin/Vista/IngresarInsidente.xaml.cs
+++ SDEAXamarin/SDEAXamarin/SDEAXamarin/Vista/IngresarInsidente.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using SDEAXamarin.Model;
+using SDEAXamarin.VistaModelo;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,5 +19,54 @@ namespace SDEAXamarin.Vista
         {
             InitializeComponent();
         }
+
+        private async void btnGuardar_Clicked(object sender, EventArgs e)
+        {
+          await insertar_operador();
+        }
+
+
+        
+        private async Task insertar_operador()
+        {
+            try
+            {
+                //var fechaactual = DateTime.Now;
+                //var fecha_nacimiento = Convert.ToDateTime(dpFechaNacimiento.Date);
+                //string edad_calculada = (fechaactual - fecha_nacimiento).ToString();
+                //txtEdad.Text = edad_calculada;
+                VMOperador operador = new VMOperador();
+                MOperador parametros = new MOperador();
+
+                parametros.Accion = txtAccion.Text;
+                parametros.Cedula=txtCedula.Text;
+                parametros.Descripcion = txtDescripcion.Text;
+                parametros.Edad=txtEdad.Text;
+                parametros.Eps = pEps.SelectedItem.ToString();
+                parametros.Evento=txtEvento.Text;
+                parametros.FechaNacimiento = dpFechaNacimiento.Date.ToString();
+                parametros.Nombre = txtNombre.Text;
+                parametros.Telefono = txtTelefono.Text;
+
+                await operador.insertar_operador(parametros);
+                await DisplayAlert("REPORTE DE INCIDENTE", "Registro Guardado Satisfactoriamente", "OK");
+                txtAccion.Text = "";
+                txtCedula.Text="";
+                txtDescripcion.Text="";
+                pEps.SelectedIndex=0;
+                txtEvento.Text="";
+                txtNombre.Text="";
+                txtTelefono.Text = "";
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+               
+            }
+            
+
+
+        }
+
     }
 }
