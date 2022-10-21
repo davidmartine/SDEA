290e76a8a6589ddafe667fdf0d3e0022d1744357
Juliangomez99 <113267974+Juliangomez99@users.noreply.github.com>
2022-10-20 19:47:20 -0500

    Se añade la interfaz de consultor

---
+++ SDEAXamarin/SDEAXamarin/SDEAXamarin/Vista/Consultor.xaml.cs
@@ -0,0 +1,42 @@
+﻿using SDEAXamarin.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SDEAXamarin.Vista
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+
+    public partial class Consultor : ContentPage
+    {
+
+        public IList<MmConsultor> consultors { get; private set; }
+        public Consultor()
+        {
+            InitializeComponent();
+            consultors = new List<MmConsultor>();
+            consultors.Add(new MmConsultor()
+            {
+                Tipo = "CONSULTAR",
+                Descripcion = "CONSULTAR INCIDENTES",
+                Imagen = "registrosmedicos.png"
+            });
+
+            BindingContext = this;
+        }
+
+        private void ListView_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            MmConsultor consultor = e.Item as MmConsultor;
+            if (consultor.Tipo.Equals("CONSULTAR"))
+            {
+                Navigation.PushAsync(new MostrarIncidentes());
+            }
+        }
+    }
+}
