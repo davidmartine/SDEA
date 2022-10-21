119b030322e651e0c48ddbaf7b8f58abccff9586
Juliangomez99 <113267974+Juliangomez99@users.noreply.github.com>
2022-10-20 19:48:21 -0500

    Modelo de Consultor

---
+++ SDEAXamarin/SDEAXamarin/SDEAXamarin/Model/MmConsultor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SDEAXamarin.Model
+{
+    public class MmConsultor
+    {
+        public string Descripcion { get; set; }
+        public string Imagen { get; set; }
+        public string Tipo { get; set; }
+    }
+}
